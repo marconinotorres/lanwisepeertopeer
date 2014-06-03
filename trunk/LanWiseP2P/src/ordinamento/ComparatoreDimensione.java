791d0845b2e8af431fc3f4f95f491eca4db69f2f
marco.cortesi01@universitadipavia.it <marco.cortesi01@universitadipavia.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-06-03 14:27:33 +0000

    Piccola modifica

--- trunk/LanWiseP2P/src/ordinamento/ComparatoreDimensione.java
+++ trunk/LanWiseP2P/src/ordinamento/ComparatoreDimensione.java
@@ -2,18 +2,20 @@ package ordinamento;
 
 import java.util.Comparator;
 
+import modello.ModelFile;
+
 /**
  * Comparatore per dimensione del file
  * 
  * @author Giuseppe Restivo
  *
  */
-public class ComparatoreDimensione implements Comparator<ModelListaFile> {
+public class ComparatoreDimensione implements Comparator<ModelFile> {
 
 	
 	
 	@Override
-	public int compare(ModelListaFile file1, ModelListaFile file2) {
+	public int compare(ModelFile file1, ModelFile file2) {
 		
 		double size1 = file1.getDimensioneFile();
 		double size2 = file2.getDimensioneFile();
