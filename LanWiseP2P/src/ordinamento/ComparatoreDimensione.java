81675634edf2c58c168a3f58484ff305067c5321
giuseppe.restivo01@ateneopv.it <giuseppe.restivo01@ateneopv.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-06-04 15:18:20 +0000

    Modificato package ordinamento

--- LanWiseP2P/src/ordinamento/ComparatoreDimensione.java
+++ LanWiseP2P/src/ordinamento/ComparatoreDimensione.java
@@ -20,7 +20,7 @@ public class ComparatoreDimensione implements Comparator<FileHandler> {
 		double size1 = file1.getDimensioneFile();
 		double size2 = file2.getDimensioneFile();
 		if(size1 <= size2){
-			return 0;
+			return -1;
 		}else{
 			return 1;
 		}
---
+++ LanWiseP2P/src/ordinamento/ProxyOrdinamento.java
@@ -0,0 +1,39 @@
+package ordinamento;
+
+import java.util.ArrayList;
+import java.util.Observable;
+
+import modello.FileHandler;
+
+/**
+ * Proxy, cambia a run-time l'istanza delle varie tipologie di filtro
+ * @author Giuseppe Restivo
+ *
+ */
+public class ProxyOrdinamento extends Observable implements IFiltro{
+
+	private IFiltro filtro;
+	
+	public void setFiltro(IFiltro filtro) {
+		this.filtro = filtro;
+		update();
+	}
+	
+	public ProxyOrdinamento(IFiltro filtro) {
+		super();
+		this.filtro = filtro;
+		update();
+	}
+
+	@Override
+	public ArrayList<FileHandler> filtraListaFile(ArrayList<FileHandler> filePresenti) {
+		return filtro.filtraListaFile(filePresenti);
+	}
+	
+	
+	
+	public void update(){
+		setChanged();
+		notifyObservers();
+	}
+}
