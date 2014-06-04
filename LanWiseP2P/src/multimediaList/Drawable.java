f62e576aecf956b50a9331e4164cc6ea66f33bc1
giuseppe.restivo01@ateneopv.it <giuseppe.restivo01@ateneopv.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-06-04 14:45:58 +0000

    Cancellato package MultimediaList

--- LanWiseP2P/src/multimediaList/Drawable.java
+++
@@ -1,9 +0,0 @@
-package multimediaList;
-
-import java.awt.Graphics;
-
-public interface Drawable {
-
-	public abstract void draw(Graphics g);
-
-}
--- LanWiseP2P/src/multimediaList/IUpdate.java
+++
@@ -1,9 +0,0 @@
-package multimediaList;
-
-import javax.swing.JPanel;
-
-public interface IUpdate {
-
-	public abstract void update();
-
-}
