e55203b7f3fea861c9d7cd2cba2782ec3f09bcc7
giuseppe.restivo01@ateneopv.it <giuseppe.restivo01@ateneopv.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-06-04 15:18:20 +0000

    Modificato package ordinamento

--- trunk/LanWiseP2P/src/ordinamento/FiltraPerDimensioneFile.java
+++ trunk/LanWiseP2P/src/ordinamento/FiltraPerDimensioneFile.java
@@ -5,6 +5,7 @@ import java.util.Collections;
 
 import modello.FileHandler;
 
+
 /**
  * Organizza la lista di File in ordine crescente della dimensione dei file, usando il Comparator sviluppato nella classe 
  * 
@@ -14,7 +15,8 @@ import modello.FileHandler;
 public class FiltraPerDimensioneFile implements IFiltro {
 
 	@Override
-	public ArrayList<FileHandler> filtraListaFile(ArrayList<FileHandler> filePresenti) {
+	public ArrayList<FileHandler> filtraListaFile(
+			ArrayList<FileHandler> filePresenti) {
 		Collections.sort(filePresenti, new ComparatoreDimensione());
 		return filePresenti;
 	}
