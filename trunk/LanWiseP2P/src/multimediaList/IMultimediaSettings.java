318631a300d20b37debf2ddfaecd232e8f245440
giuseppe.restivo01@ateneopv.it <giuseppe.restivo01@ateneopv.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-06-04 14:45:58 +0000

    Cancellato package MultimediaList

--- trunk/LanWiseP2P/src/multimediaList/IMultimediaSettings.java
+++
@@ -1,9 +0,0 @@
-package multimediaList;
-
-import java.awt.Graphics;
-
-public interface IMultimediaSettings {
-
-	public abstract void setting(Graphics g, int i, int index);
-
-}
--- trunk/LanWiseP2P/src/multimediaList/IPlacer.java
+++
@@ -1,7 +0,0 @@
-package multimediaList;
-
-public interface IPlacer {
-
-	public abstract void place(int[] xy, int index, int i);
-
-}
