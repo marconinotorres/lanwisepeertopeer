c2c68c637ddcfb6861eb527ac4337099e5649cbf
federico.bacci01@ateneopv.it <federico.bacci01@ateneopv.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-06-07 19:32:27 +0000

    (Commit dell'altra parte di gestione della rete scritta da me e Blago) Abbiamo creato le classi PeerAsClient, PeerAsServer, Client e Server che, dopo aver inviato e ricevuto il nome del file richiesto, gestiscono il flusso di dati e trasferiscono effettivamente il file. Inoltre abbiamo scritto le classi BroadcastClient e BroadcastServer che continuano ad inviare sulla rete la lista dei file nella propria cartella condivisa e ricevono ogni *da definire* secondi la lista di tutti i file in tutte le cartelle condivise degli utenti, così poi da poterli visualizzare e (nel caso) selezionare e scaricare. La classe Utente è semplicemente un contenitore di attributi cruciali quali il percorso della cartella condivisa e quello della cartella di download.

---
+++ LanWiseP2P/src/reti/PeerAsClient.java
@@ -0,0 +1,51 @@
+package reti;
+
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+public class PeerAsClient extends Thread {
+	
+	public static int SOCKET_PORT_REQUEST = 13697;
+	public String IP_SERVER;
+	public String FILE_TO_REQUEST;
+	public String FILE_TO_RECEIVED;
+	
+	public void run(){ //probabilmente c'?? un mdoo migliore di farlo;
+		try {
+			apriConnessione();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void apriConnessione() throws Exception{
+		OutputStreamWriter osw;
+		Socket sock = null;
+
+		sock = new Socket(IP_SERVER, SOCKET_PORT_REQUEST);
+		System.out.println("(PC) Connecting...");
+
+//		os = sock.getOutputStream();
+//		PrintWriter out = new PrintWriter(os);
+//		out.print(FILE_TO_REQUEST);
+		osw = new OutputStreamWriter(sock.getOutputStream(), "UTF-8");
+        osw.write(FILE_TO_REQUEST, 0, FILE_TO_REQUEST.length());
+		
+		osw.flush();
+		osw.close();
+		
+		sleep(0);
+//		assert Boolean.TRUE;
+
+		sock.close();
+
+		Client client = new Client();
+		client.FILE_TO_RECEIVED = FILE_TO_RECEIVED;
+		client.IP_SERVER = IP_SERVER;
+		client.start();
+		client.join();
+		
+		
+	}
+
+}
