cb9fd9720538fb6b238c9bbdeaf52a405ac2423d
marco.cortesi01@universitadipavia.it <marco.cortesi01@universitadipavia.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-06-10 10:58:20 +0000

    Piccola modifica LookAndFeel

--- LanWiseP2P/src/tests/Main.java
+++ LanWiseP2P/src/tests/Main.java
@@ -3,8 +3,9 @@ package tests;
 import java.awt.Dimension;
 import java.awt.Toolkit;
 
-import javax.activation.MailcapCommandMap;
 import javax.swing.JFrame;
+import javax.swing.UIManager;
+import javax.swing.UnsupportedLookAndFeelException;
 
 import modello.PannelloAccesso;
 
@@ -19,7 +20,23 @@ public class Main {
 	}
 	public static void main(String[] args) {
 		
+		
 		Main.frame=new JFrame("LWP2P");
+		try {
+			UIManager.setLookAndFeel( UIManager.getSystemLookAndFeelClassName() );
+		} catch (ClassNotFoundException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (InstantiationException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (IllegalAccessException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		} catch (UnsupportedLookAndFeelException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
 		PannelloAccesso p=new PannelloAccesso();
 		frame.getContentPane().add(p);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
