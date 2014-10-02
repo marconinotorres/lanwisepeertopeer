37831628f7a0395da0d9295ccce02a72c3736931
giuseppe.restivo01@universitadipavia.it <giuseppe.restivo01@universitadipavia.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-10-02 14:30:33 +0000

    Piccola modifica

--- LanWiseP2P/src/modello/IUtenti.java
+++ LanWiseP2P/src/modello/IUtenti.java
@@ -1,11 +1,16 @@
 package modello;
 
-import java.util.List;
-
+/**
+* 
+* Possibile gestione con Basi di Dati o tramite una lettura da file
+* 
+* @author giusepperestivo
+*
+*/
 public interface IUtenti {
 
 	/**
 	 * @return una lista di utenti che possono accedere all'applicazione
 	 */
-	public List<Utente> getUtenti();
+	public void getUtenti();
 }
