5fb75e0cb4595671289b7c0712475a4398a67fc2
giuseppe.restivo01@universitadipavia.it <giuseppe.restivo01@universitadipavia.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-10-02 20:18:29 +0000

    Piccola Modifica

--- trunk/LanWiseP2P/src/modello/readUtenti/TestReadUtenti.java
+++ trunk/LanWiseP2P/src/modello/readUtenti/TestReadUtenti.java
@@ -9,27 +9,38 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
-import modello.IUtenti;
 import modello.Utente;
 
-public class TestReadUtenti implements IUtenti{
+/**
+ * 0 -> non è connesso
+ * 1 -> è connesso
+ * @author giusepperestivo
+ *
+ */
+public class TestReadUtenti extends UtenteReader{
 
-	/*
-	 * (non-Javadoc)
-	 * @see modello.IUtenti#getUtenti()
-	 */
 	@Override
-	public void getUtenti() {
+	public List<Utente> readFileUtente() {
+		
+		List<Utente> lista = new ArrayList<>();
+
 		File file = new File("data/listaUtenti.txt");
 		
 		try {
+			
 			BufferedReader reader = new BufferedReader(new FileReader(file));
 			String line = reader.readLine(); 
 			while (line!=null) {
+				
 				StringTokenizer token = new StringTokenizer(line);
 				String nome = token.nextToken();
-				String cognome = nome + token.nextToken();
-				Utente.getUtente().addUtente(cognome);
+				String cognome = token.nextToken();
+				String conn = token.nextToken();
+				
+				if (conn.equals("N")) {
+					lista.add(new Utente(nome, cognome));
+				}
+				
 				line = reader.readLine();
 			}
 			reader.close();
@@ -40,5 +51,8 @@ public class TestReadUtenti implements IUtenti{
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+
+		return lista;
+
 	}
 }
