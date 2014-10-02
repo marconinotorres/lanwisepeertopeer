83256411a8280e1a8c9ce071c230c7f7083b223d
giuseppe.restivo01@universitadipavia.it <giuseppe.restivo01@universitadipavia.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-10-02 20:21:58 +0000

    Piccola Modifica

--- trunk/LanWiseP2P/src/grafica/PannelloAccesso.java
+++ trunk/LanWiseP2P/src/grafica/PannelloAccesso.java
@@ -5,6 +5,7 @@ import java.awt.GridLayout;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.ButtonGroup;
 import javax.swing.JPanel;
@@ -12,6 +13,9 @@ import javax.swing.JRadioButton;
 import javax.swing.border.TitledBorder;
 
 import modello.Utente;
+import modello.readUtenti.TestReadUtenti;
+import modello.readUtenti.UtenteReader;
+import modello.writeUtenti.UtenteWriter;
 import reti.CartelleUtente;
 import reti.PeerAsServer;
 import tests.Main;
@@ -20,37 +24,38 @@ import tests.Main;
 public class PannelloAccesso extends JPanel {
 	
 	private static final long serialVersionUID = 0;
-		
-	private final Utente listaUtenti;
+	
 	private final ArrayList<JRadioButton> buttonUtenti=new ArrayList<>();
 	
-	public PannelloAccesso(final Utente listaUtenti){
+	public PannelloAccesso(){
 		
-		this.listaUtenti = listaUtenti;
 		setLayout(new GridLayout(5,1));
 		TitledBorder border =new TitledBorder("Lista_Utenti");
 		border.setTitleColor(Color.blue);
 		setBorder(border);
 		ButtonGroup gruppo=new ButtonGroup();
 		
-		for (int i = 0; i < 5; i++) {
-			
-			buttonUtenti.add(new JRadioButton(listaUtenti.getListaUtenti().get(i)));
+		for (int i = 0; i < UtenteReader.getUtente().readFileUtente().size(); i++) {
+			String nome = UtenteReader.getUtente().readFileUtente().get(i).getNome();
+			String cognome = UtenteReader.getUtente().readFileUtente().get(i).getCognome();
+			buttonUtenti.add(new JRadioButton(nome+"  "+cognome));
 			gruppo.add(buttonUtenti.get(i));
 			add(buttonUtenti.get(i));
 			
 			
-//			System.err.println(utenti.get(i).getText());
 		}
+		List<Utente> lista = UtenteReader.getUtente().readFileUtente();
 		
 		for (int i = 0; i < buttonUtenti.size(); i++) {
-			final String utenteB = listaUtenti.getListaUtenti().get(i);
+			final String utenteCollegato = lista.get(i).getCognome();
+			
 			buttonUtenti.get(i).addActionListener(new ActionListener() {
 				
 				@Override
 				public void actionPerformed(ActionEvent arg0) {
-					listaUtenti.setUtenteSelezionato(utenteB);
-					listaUtenti.controlloAccessi(utenteB);
+					UtenteWriter.getUtente().setUtenteCollegato(utenteCollegato);
+					UtenteWriter.getUtente().writeConnessione(utenteCollegato,"S");
+					
 					FrameCaricaCartella cartella = new FrameCaricaCartella();
 					Main.getFrame().dispose();
 					CartelleUtente utente = CartelleUtente.getInstance();
