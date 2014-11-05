12c604750b46cb592a74a501bab87100f4bea01d
giuseppe.restivo01@universitadipavia.it <giuseppe.restivo01@universitadipavia.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-11-05 17:52:31 +0000

    Modificato un test

--- LanWiseP2P/src/tests/TestSendFiles.java
+++ LanWiseP2P/src/tests/TestSendFiles.java
@@ -11,9 +11,9 @@ public class TestSendFiles {
 		sapo.start();
 		
 		PeerAsClient stePac = new PeerAsClient();
-		stePac.IP_SERVER = "127.0.0.1";
-		stePac.FILE_TO_REQUEST = "/home/rant/Scrivania/testS/cell";
-		stePac.FILE_TO_RECEIVED = "/home/rant/Scrivania/testS/cell2";
+		stePac.setIP_SERVER("127.0.0.1");
+		stePac.setFILE_TO_REQUEST("/home/rant/Scrivania/testS/cell");
+		stePac.setFILE_TO_RECEIVED("/home/rant/Scrivania/testS/cell2");
 		stePac.start();
 		try {
 			stePac.join();
