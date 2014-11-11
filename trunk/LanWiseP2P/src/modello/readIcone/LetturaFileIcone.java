81a32ab1848cc6a8e8d7e65a91e8f1f82866fe19
giuseppe.restivo01@universitadipavia.it <giuseppe.restivo01@universitadipavia.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-11-11 09:12:52 +0000

    Creato package modello.readIcone e inserite due set di icone

---
+++ trunk/LanWiseP2P/src/modello/readIcone/LetturaFileIcone.java
@@ -0,0 +1,56 @@
+package modello.readIcone;
+
+import java.io.File;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Observable;
+
+import modello.Icona;
+
+
+public class LetturaFileIcone {
+
+	private static LetturaFileIcone letturaIcone = new LetturaFileIcone();
+	private LetturaFileIcone() {
+		// TODO Auto-generated constructor stub
+	}
+	
+	public static LetturaFileIcone getLetturaIcone() {
+		return letturaIcone;
+	}
+	
+	
+	private ISetIcone setIcone;
+	private String path;
+	public void setSetIcone(ISetIcone setIcone) {
+		this.setIcone = setIcone;
+	}
+	
+	public ISetIcone getSetIcone() {
+		return setIcone;
+	}
+	public String getPath(){
+		return path;
+	}
+	public void setPath(String path){
+		this.path = path;
+	}
+	
+	/**
+	 * Tutti i set di icone verranno aggiunte automaticamente alla lista dei set disponibili
+	 * @return i nomi di tutti i set di icone disponibili dell'applicazione
+	 */
+	public String[] setIcone(){
+		List<String> listaIcone = new ArrayList<String>();
+		File file = new File("icone");
+		File[] lista = file.listFiles();
+		for (int i = 0; i < lista.length; i++) {
+			if (lista[i].isDirectory()) {
+				listaIcone.add(lista[i].getName());
+			}
+		}
+		return listaIcone.toArray(new String[listaIcone.size()]);
+	}
+	
+	
+}
