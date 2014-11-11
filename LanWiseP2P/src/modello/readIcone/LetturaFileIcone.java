7949b4afa96384fe46a1e7b9982eacf5e207fc39
giuseppe.restivo01@universitadipavia.it <giuseppe.restivo01@universitadipavia.it@af25ca92-ccdf-c73b-da4b-d87bfb02aaea>
2014-11-11 09:47:03 +0000

    Ci siamo quasi per la parte grafica, adesso è possibile cambiare set di icone a run-time

--- LanWiseP2P/src/modello/readIcone/LetturaFileIcone.java
+++ LanWiseP2P/src/modello/readIcone/LetturaFileIcone.java
@@ -5,6 +5,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.Observable;
 
+import modello.ISetIcone;
 import modello.Icona;
 
 
--- LanWiseP2P/src/modello/readIcone/TestLetturaIcone.java
+++ LanWiseP2P/src/modello/readIcone/TestLetturaIcone.java
@@ -9,6 +9,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.StringTokenizer;
 
+import modello.ISetIcone;
 import modello.Icona;
 
 public class TestLetturaIcone implements ISetIcone{
